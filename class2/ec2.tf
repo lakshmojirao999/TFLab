42d48f63ff2e306a407efdd55b9d33dd79166b42
uma <umaatturu>
2022-06-24 15:53:18 +0530

    terraform

---
+++ class2/ec2.tf
@@ -0,0 +1,43 @@
+variable "image" {
+    default = "ami-0cff7528ff583bf9a"
+
+}
+
+variable "instance_type" {
+    default = "t3.medium"
+
+}
+
+variable "region" {
+    default = "us-east-1"
+
+}
+
+variable "access_key" {
+    
+
+}
+
+variable "secret_key" {
+  
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAS6FURPIRE4LNP5WQ"
+  secret_key = "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"
+}
+
+
+resource "aws_instance" "intro" {
+  ami           = var.image
+  instance_type = var.instance_type
+  subnet_id     = "subnet-0405a9c8c1a899c4a"
+  vpc_security_group_ids = ["sg-075d9f9a89617f250"]
+     tags = {
+    Name = "terraform"
+  }    
+
+} 
+
+  
