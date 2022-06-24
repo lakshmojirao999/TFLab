fc030eaf5cab5e5ab7a4401a38476e76fa356d12
Davidpavan <david@zelarsoft.com>
2022-06-24 15:18:31 +0530

    Terraform-ec2-instance

--- day2/main.tf
+++ day2/main.tf
@@ -1,39 +1,55 @@
 variable "image" {
-    default = "ami-098e42ae54c764c35"
+    default = "ami-052efd3df9dad4825"
   
 }
 
 variable "instance_type" {
-    default = "t3.micro"
+    default = "t3.medium"
+  
+}
+
+# variable "subnet" {
+#     default = "subnet-ae88b0e3"
+  
+# }
+
+variable "sg" {
+    default = ["sg-37809036"]
   
 }
 
 variable "region" {
-    default = "us-west-2"
+    default = "us-east-1"
   
 }
 
+
 variable "access_key" {
-  
+  default = "AKIATHIV3VM3P2J23SXR"
 }
 
 variable "secret_key" {
-  
+  default = "qhib88bR0EZf1F588j4jEn/cfmraiMlTa0B51bNh"
 }
 
 provider "aws" {
   region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 
 
-resource "aws_instance" "sandbox" {
+resource "aws_instance" "Terraform" {
   ami           = var.image
   instance_type = var.instance_type
+  subnet_id     = "subnet-ae88b0e3"
+  security_groups = var.sg
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Terraform"
   }    
   
+}
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.Terraform.id
+  vpc      = true
 }
