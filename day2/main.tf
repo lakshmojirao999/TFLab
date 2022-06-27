fa5f360a78f99e0ae3435a8758d5005c077be006
NithyaParavasthu <nithyap0104@gmail.com>
2022-06-27 22:46:46 +0530

    day 2 main.tf

--- day2/main.tf
+++ day2/main.tf
@@ -1,39 +1,39 @@
-variable "image" {
-    default = "ami-098e42ae54c764c35"
+# variable "image" {
+#     default = "ami-098e42ae54c764c35"
   
-}
+# }
 
-variable "instance_type" {
-    default = "t3.micro"
+# variable "instance_type" {
+#     default = "t3.micro"
   
-}
+# }
 
-variable "region" {
-    default = "us-west-2"
+# variable "region" {
+#     default = "us-west-2"
   
-}
+# }
 
-variable "access_key" {
+# variable "access_key" {
   
-}
+# }
 
-variable "secret_key" {
+# variable "secret_key" {
   
-}
+# }
 
-provider "aws" {
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
+# provider "aws" {
+#   region     = var.region
+#   access_key = var.access_key
+#   secret_key = var.secret_key
+# }
 
 
-resource "aws_instance" "sandbox" {
-  ami           = var.image
-  instance_type = var.instance_type
+# resource "aws_instance" "sandbox" {
+#   ami           = var.image
+#   instance_type = var.instance_type
 
-  tags = {
-    Name = "HelloWorld"
-  }    
+#   tags = {
+#     Name = "HelloWorld"
+#   }    
   
-}
+# }
